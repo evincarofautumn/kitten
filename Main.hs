ac127b0bc04402b9d421c77f3413116a47bcc828
Jon Purdy <evincarofautumn@gmail.com>
2012-12-07 21:37:22 -0800

    Tokenize words.

---
+++ Main.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Main where
+
+import Control.Applicative
+import Data.Function
+import System.IO
+
+import qualified Text.Parsec as P
+
+main :: IO ()
+main = fix $ \ loop -> do
+  putStr "> "
+  hFlush stdout
+  line <- getLine
+  case line of
+    ":q" -> return ()
+    _ -> do
+      putStrLn $ case compile "STDIN" line of
+        Left compileError -> show compileError
+        Right compileResult -> show compileResult
+      loop
+
+data CompileError
+  = CompileError String
+
+instance Show CompileError where
+  show (CompileError message) = message
+
+data Token
+  = Token String
+
+instance Show Token where
+  show (Token token) = token
+
+compile :: String -> String -> Either CompileError [Token]
+compile name source
+  = case tokenize name source of
+    Left error -> Left $ CompileError (show error)
+    Right result -> Right result
+
+tokenize :: String -> String -> Either P.ParseError [Token]
+tokenize name source = P.parse file name source
+
+file :: (P.Stream s m Char) => P.ParsecT s u m [Token]
+file = tokens <* P.eof
+
+tokens :: (P.Stream s m Char) => P.ParsecT s u m [Token]
+tokens = token `P.sepBy` P.spaces
+
+token :: (P.Stream s m Char) => P.ParsecT s u m Token
+token = Token <$> P.many1
+  (P.letter <|> P.digit <|> P.char '_')
