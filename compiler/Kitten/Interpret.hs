324affa8bc80d1ae2f8080c610e70acf29a0cae7
Jon Purdy <evincarofautumn@gmail.com>
2013-01-26 11:31:29 -0800

    Kill redundant parentheses.

--- compiler/Kitten/Interpret.hs
+++ compiler/Kitten/Interpret.hs
@@ -32,93 +32,93 @@ interpret stack (Fragment defs body)
     _ -> pushData value
   runTerm (Builtin builtin) = case builtin of
     Builtin.Add -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Int $ a + b
     Builtin.AndBool -> do
-      (Bool b) <- popData
-      (Bool a) <- popData
+      Bool b <- popData
+      Bool a <- popData
       pushData . Bool $ a && b
     Builtin.AndInt -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Int $ a .&. b
     Builtin.Apply -> do
-      (Fun a) <- popData
+      Fun a <- popData
       runTerm a
     Builtin.Compose -> do
-      (Fun b) <- popData
-      (Fun a) <- popData
+      Fun b <- popData
+      Fun a <- popData
       pushData . Fun $ Compose a b
     Builtin.Div -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Int $ a `div` b
     Builtin.Drop -> popData_
     Builtin.Dup -> pushData =<< peekData
     Builtin.Eq -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Bool $ a == b
     Builtin.Fun -> do
       a <- popData
       pushData . Fun $ Value a
     Builtin.Ge -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Bool $ a >= b
     Builtin.Gt -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Bool $ a > b
     Builtin.If -> do
-      (Bool condition) <- popData
-      (Fun false) <- popData
-      (Fun true) <- popData
+      Bool condition <- popData
+      Fun false <- popData
+      Fun true <- popData
       runTerm $ if condition then true else false
     Builtin.Le -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Bool $ a <= b
     Builtin.Lt -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Bool $ a < b
     Builtin.Mod -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Int $ a `mod` b
     Builtin.Mul -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Int $ a * b
     Builtin.Ne -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Bool $ a /= b
     Builtin.Neg -> do
-      (Int a) <- popData
+      Int a <- popData
       pushData . Int $ negate a
     Builtin.NotBool -> do
-      (Bool a) <- popData
+      Bool a <- popData
       pushData . Bool $ not a
     Builtin.NotInt -> do
-      (Int a) <- popData
+      Int a <- popData
       pushData . Int $ complement a
     Builtin.OrBool -> do
-      (Bool b) <- popData
-      (Bool a) <- popData
+      Bool b <- popData
+      Bool a <- popData
       pushData . Bool $ a || b
     Builtin.OrInt -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Int $ a .|. b
     Builtin.Print -> do
-      (Text s) <- popData
+      Text s <- popData
       lift $ Text.putStr s
     Builtin.Sub -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Int $ a - b
     Builtin.Swap -> do
       b <- popData
@@ -126,12 +126,12 @@ interpret stack (Fragment defs body)
       pushData b
       pushData a
     Builtin.XorBool -> do
-      (Bool b) <- popData
-      (Bool a) <- popData
+      Bool b <- popData
+      Bool a <- popData
       pushData . Bool $ a /= b
     Builtin.XorInt -> do
-      (Int b) <- popData
-      (Int a) <- popData
+      Int b <- popData
+      Int a <- popData
       pushData . Int $ a `xor` b
   runTerm (Scoped term) = do
     pushLocal =<< popData
