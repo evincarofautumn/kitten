3f1dd5e82aa3d5acda0b4d73f14d5a4c1e63f30b
Jon Purdy <evincarofautumn@gmail.com>
2013-05-07 23:15:45 -0700

    Split interpretation into submodules.

--- lib/Kitten/Interpret.hs
+++ lib/Kitten/Interpret.hs
@@ -13,22 +13,11 @@ import Data.Fixed
 import Kitten.Builtin (Builtin)
 import Kitten.Def
 import Kitten.Fragment
-import Kitten.Location
+import Kitten.Interpret.Monad
 import Kitten.Name
 import Kitten.Resolved
 
 import qualified Kitten.Builtin as Builtin
-
-data Env = Env
-  { envData :: [Value]
-  , envLocals :: [Value]
-  , envDefs :: [Def Resolved]
-  , envClosure :: [Value]
-  , envLocations :: [Location]
-  }
-
-type InterpretM a = StateT Env IO a
-type Interpret = InterpretM ()
 
 interpret :: [Value] -> Fragment Resolved -> IO ()
 interpret stack Fragment{..} = void $ evalStateT
@@ -248,62 +237,3 @@ interpretBuiltin builtin = case builtin of
     Int b <- popData
     Int a <- popData
     pushData $ Int (f a b)
-
-pushData :: Value -> Interpret
-pushData value = modify $ \ env@Env{..}
-  -> env { envData = value : envData }
-
-popData :: InterpretM Value
-popData = do
-  dataStack <- gets envData
-  case dataStack of
-    [] -> fail "stack underflow"
-    (top : down) -> do
-      modify $ \ env -> env { envData = down }
-      return top
-
-getLocal :: Name -> InterpretM Value
-getLocal (Name index) = do
-  locals <- gets envLocals
-  return $ locals !! index
-
-getClosed :: Name -> InterpretM Value
-getClosed (Name index) = do
-  closure <- gets envClosure
-  return $ closure !! index
-
-pushLocal :: Value -> Interpret
-pushLocal value = modify $ \ env@Env{..}
-  -> env { envLocals = value : envLocals }
-
-popLocal :: Interpret
-popLocal = do
-  localStack <- gets envLocals
-  case localStack of
-    [] -> fail "local stack underflow"
-    (_ : down) -> do
-      modify $ \ env -> env { envLocals = down }
-
-withClosure :: [Value] -> InterpretM a -> InterpretM a
-withClosure values action = do
-  closure <- gets envClosure
-  modify $ \ env@Env{..} -> env { envClosure = values }
-  result <- action
-  modify $ \ env@Env{..} -> env { envClosure = closure }
-  return result
-
-here :: InterpretM Location
-here = do
-  locations <- gets envLocations
-  return $ case locations of
-    [] -> UnknownLocation
-    (location : _) -> location
-
-withLocation :: Location -> InterpretM a -> InterpretM a
-withLocation location action = do
-  modify $ \ env@Env{..} -> env
-    { envLocations = location : envLocations }
-  result <- action
-  modify $ \ env@Env{..} -> env
-    { envLocations = tail envLocations }
-  return result
