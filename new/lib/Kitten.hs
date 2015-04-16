e7024a4de5f2c3d552f0f143d2f39cd2220b02e7
Jon Purdy <evincarofautumn@gmail.com>
2015-04-16 12:03:25 -0700

    WIP: Rearchitect.
    Reorganise the code, begin porting the old parser, updating as we go.

--- new/lib/Kitten.hs
+++ new/lib/Kitten.hs
@@ -1,4 +1,30 @@
-module Kitten where
+{-# LANGUAGE OverloadedStrings #-}
 
-meow :: IO ()
-meow = putStrLn "meow"
+module Kitten
+  ( Expr(..)
+  , Name(..)
+  , Program(..)
+  , Type(..)
+  , emptyProgram
+  , programToC
+  ) where
+
+import Data.Map (Map)
+import Data.Text (Text)
+import Data.Vector (Vector)
+import qualified Data.Map as Map
+
+newtype Name = Name { nameComponents :: Vector Text }
+
+newtype Program = Program { programDefs :: Map Name (Type, Expr) }
+
+data Type = Type
+data Expr = Expr
+
+
+
+emptyProgram :: Program
+emptyProgram = Program Map.empty
+
+programToC :: Program -> Text
+programToC _program = "butts"
