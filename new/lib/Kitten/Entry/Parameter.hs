8d7f3a4948b95eef8aac342bd99ad9e40ac45b7d
Jon Purdy <evincarofautumn@gmail.com>
2016-03-15 23:27:20 -0700

    Improve //info output.

--- new/lib/Kitten/Entry/Parameter.hs
+++ new/lib/Kitten/Entry/Parameter.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Kitten.Entry.Parameter
   ( Parameter(..)
   ) where
 
-import Kitten.Kind (Kind)
+import Kitten.Kind (Kind(..))
 import Kitten.Name (Unqualified)
 import Kitten.Origin (Origin)
+import Text.PrettyPrint.HughesPJClass (Pretty(..))
+import qualified Text.PrettyPrint as Pretty
 
 -- A generic type parameter for a data type.
 data Parameter = Parameter !Origin !Unqualified !Kind
@@ -13,3 +17,11 @@ data Parameter = Parameter !Origin !Unqualified !Kind
 -- Parameters are compared regardless of origin.
 instance Eq Parameter where
   Parameter _ a b == Parameter _ c d = (a, b) == (c, d)
+
+instance Pretty Parameter where
+  pPrint (Parameter _ name kind) = case kind of
+    Value -> pPrint name
+    Stack -> Pretty.hcat [pPrint name, "..."]
+    Label -> Pretty.hcat ["+", pPrint name]
+    Permission -> Pretty.hcat ["+", pPrint name]
+    _ :-> _ -> Pretty.hcat [pPrint name, "<_>"]
